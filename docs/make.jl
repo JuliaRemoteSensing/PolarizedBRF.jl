506affc7979db0a3066fdba14c06be2314e4ca17
Gabriel Wu <wuzihua@pku.edu.cn>
2022-07-12 08:59:20 +0800

    chore: move to JuliaRemoteSensing

--- docs/make.jl
+++ docs/make.jl
@@ -6,11 +6,11 @@ DocMeta.setdocmeta!(PBRF, :DocTestSetup, :(using PBRF); recursive=true)
 makedocs(;
     modules=[PBRF],
     authors="Gabriel Wu <wuzihua@pku.edu.cn> and contributors",
-    repo="https://github.com/lucifer1004/PBRF.jl/blob/{commit}{path}#{line}",
+    repo="https://github.com/JuliaRemoteSensing/PBRF.jl/blob/{commit}{path}#{line}",
     sitename="PBRF.jl",
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://lucifer1004.github.io/PBRF.jl",
+        canonical="https://JuliaRemoteSensing.github.io/PBRF.jl",
         edit_link="main",
         assets=String[],
     ),
@@ -20,6 +20,6 @@ makedocs(;
 )
 
 deploydocs(;
-    repo="github.com/lucifer1004/PBRF.jl",
+    repo="github.com/JuliaRemoteSensing/PBRF.jl",
     devbranch="main",
 )
--- docs/src/index.md
+++ docs/src/index.md
@@ -4,7 +4,7 @@ CurrentModule = PBRF
 
 # PBRF
 
-Documentation for [PBRF](https://github.com/lucifer1004/PBRF.jl).
+Documentation for [PBRF](https://github.com/JuliaRemoteSensing/PBRF.jl).
 
 ```@index
 ```
